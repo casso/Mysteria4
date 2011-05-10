7e40e20c47cce5797e9d3ced6232f2011e9a7dee
Casso <CassoBiker@GMail.Com>
2011-05-10 21:26:40 +0200

    BG announce Signed-off-by: Casso <CassoBiker@GMail.Com>

---
+++ sql/Custom Patches/000_22_mangos_string.sql
@@ -0,0 +1,1 @@
+UPDATE `mangos_string` SET `content_default`='|cffffffffBattle for %s started! Join this battleground.\r\n|cff00ccffAlliance players: %u |cffff0000Horde players: %s|r' WHERE `entry`='717' ;
