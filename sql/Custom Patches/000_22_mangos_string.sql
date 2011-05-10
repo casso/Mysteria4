5828bd66a276bad8bd257f28f07ed9ebba04d87d
Casso <CassoBiker@GMail.Com>
2011-05-10 21:36:38 +0200

    Typo u predchadzajuceho commitu Signed-off-by: Casso <CassoBiker@GMail.Com>

--- sql/Custom Patches/000_22_mangos_string.sql
+++ sql/Custom Patches/000_22_mangos_string.sql
@@ -1,1 +1,1 @@
-UPDATE `mangos_string` SET `content_default`='|cffffffffBattle for %s started! Join this battleground.\r\n|cff00ccffAlliance players: %u |cffff0000Horde players: %s|r' WHERE `entry`='717' ;
+UPDATE `mangos_string` SET `content_default`='|cffffffffBattle for %s started! Join this battleground.\r\n|cff00ccffAlliance players: %u |cffff0000Horde players: %u|r' WHERE `entry`='717' ;
