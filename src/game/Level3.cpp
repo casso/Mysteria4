3e4e80c47e58222a250fc9fc0a286f60f6e8f36b
Casso <CassoBiker@GMail.Com>
2011-05-15 13:47:59 +0200

    Visual bug prikazu .gm fly on/off Signed-off-by: Casso <CassoBiker@GMail.Com>

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -5208,7 +5208,7 @@ bool ChatHandler::HandleGMFlyCommand(char* args)
     data << target->GetPackGUID();
     data << uint32(0);                                      // unknown
     target->SendMessageToSet(&data, true);
-    PSendSysMessage(LANG_COMMAND_FLYMODE_STATUS, GetNameLink(target).c_str(), args);
+    PSendSysMessage(LANG_COMMAND_FLYMODE_STATUS, GetNameLink(target).c_str(), value ? "on" : "off");
     return true;
 }
 
