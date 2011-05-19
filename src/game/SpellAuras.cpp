3ab4efc6da5ec9249caf32a61008168f760c5890
Casso <CassoBiker@GMail.Com>
2011-05-19 19:31:36 +0200

    Anger managment warr talent ma procovat len v combate Signed-off-by: Casso <CassoBiker@GMail.Com>

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6559,8 +6559,9 @@ void Aura::PeriodicTick()
             // Anger Management
             // amount = 1+ 16 = 17 = 3,4*5 = 10,2*5/3 
             // so 17 is rounded amount for 5 sec tick grow ~ 1 range grow in 3 sec
-            if(pt == POWER_RAGE)
-                target->ModifyPower(pt, m_modifier.m_amount * 3 / 5);
+            if(target->isInCombat()) // while in combat
+                if(pt == POWER_RAGE)
+                    target->ModifyPower(pt, m_modifier.m_amount * 3 / 5);
             break;
         }
         // Here tick dummy auras
