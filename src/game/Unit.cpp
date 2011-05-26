5bd742cbd81ca1911dfb6b4cd364fe9bc1bf1cd2
Casso <CassoBiker@GMail.Com>
2011-05-26 17:17:17 +0200

    Implementacia SPELL_ATTR_EX3_CANT_MISS atributu. Niektore spelly sa nemaju dat "missnut" Na zaklade: https://github.com/mangos/one/commit/eb67829163c0aa63c1501f93c9222b82546e5681 Signed-off-by: Casso <CassoBiker@GMail.Com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -2582,7 +2582,7 @@ SpellMissInfo Unit::MeleeSpellHitResult(Unit *pVictim, SpellEntry const *spell)
 
     uint32 missChance = uint32(MeleeSpellMissChance(pVictim, attType, fullSkillDiff, spell)*100.0f);
     // Roll miss
-    uint32 tmp = missChance;
+    uint32 tmp = spell->AttributesEx3 & SPELL_ATTR_EX3_CANT_MISS ? 0 : missChance;
     if (roll < tmp)
         return SPELL_MISS_MISS;
 
@@ -2748,8 +2748,10 @@ SpellMissInfo Unit::MagicSpellHitResult(Unit *pVictim, SpellEntry const *spell)
     if (HitChance <  100) HitChance =  100;
     if (HitChance > 9900) HitChance = 9900;
 
+    int32 tmp = spell->AttributesEx3 & SPELL_ATTR_EX3_CANT_MISS ? 0 : (10000 - HitChance);
+
     int32 rand = irand(0,10000);
-    if (rand > HitChance)
+    if (rand < tmp)
         return SPELL_MISS_RESIST;
 
     return SPELL_MISS_NONE;
