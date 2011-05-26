a3176864ee0b5f861fceb91f75a29bc9761b21b8
Casso <CassoBiker@GMail.Com>
2011-05-26 17:09:06 +0200

    Oprava DoT bonusu u mangle spellov, z mangos-one: https://github.com/mangos/one/commit/df20b3f695d17e59b3bcb3cb24292f62a69858b8 Signed-off-by: Casso <CassoBiker@GMail.Com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7567,17 +7567,6 @@ uint32 Unit::SpellDamageBonus(Unit *pVictim, SpellEntry const *spellProto, uint3
                     TakenTotalMod *= (mod+100.0f)/100.0f;
                 }
                 break;
-            // Mangle
-            case 2312:
-                for(int j = 0; j < MAX_EFFECT_INDEX; ++j)
-                {
-                    if(GetEffectMechanic(spellProto, SpellEffectIndex(j))==MECHANIC_BLEED)
-                    {
-                        TakenTotalMod *= (100.0f+(*i)->GetModifier()->m_amount)/100.0f;
-                        break;
-                    }
-                }
-                break;
         }
     }
 
@@ -8321,10 +8310,7 @@ uint32 Unit::MeleeDamageBonus(Unit *pVictim, uint32 pdamage,WeaponAttackType att
                 break;
             //Mangle
             case 2312:
-                if(spellProto==NULL)
-                    break;
-                // Should increase Shred (initial Damage of Lacerate and Rake handled in Spell::EffectSchoolDMG)
-                if(spellProto->SpellFamilyName==SPELLFAMILY_DRUID && (spellProto->SpellFamilyFlags == UI64LIT(0x00008000)))
+                if (mechanicMask & (1 << (MECHANIC_BLEED-1)))
                     TakenPercent *= (100.0f+(*i)->GetModifier()->m_amount)/100.0f;
                 break;
         }
