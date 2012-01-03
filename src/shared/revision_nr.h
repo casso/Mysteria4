d1eb3aaaf7794c5ce66e54c8067d2cab42334ead
Casso <CassoBiker@GMail.Com>
2012-01-03 19:10:42 +0100

    029

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
-#define REVISION_NR "0870_Mysteria4_RC28_Sunwell_begun_to_open"
+#define REVISION_NR "0870_Mysteria4_RC29"
 #endif // __REVISION_NR_H__
